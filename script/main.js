13390715caef9b3945edb95cdf3a3830ef280025
FarhanVirenze <farhanvirenze18@gmail.com>
2024-03-31 16:16:41 +0700

    first commit

--- script/main.js
+++ script/main.js
@@ -1,74 +1,16 @@
-const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
-// For educational purposes only - DO NOT USE in production
-// Request your own key for free: https://developers.themoviedb.org/3
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=1`;
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
+const waktu = document.getElementById("waktu") // Mendapatkan elemen dengan ID "waktu" dari HTML
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-
-const getClassByRate = (vote) => {
-  if (vote >= 7.5) return "green";
-  else if (vote >= 7) return "orange";
-  else return "red";
-};
-
-const showMovies = (movies) => {
-  main.innerHTML = "";
-  movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
-    const movieElement = document.createElement("div");
-    movieElement.classList.add("movie");
-    movieElement.innerHTML = `
-    <img
-      src="${IMG_PATH + poster_path}"
-      alt="${title}"
-    />
-    <div class="movie-info">
-      <h3>${title}</h3>
-      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-    </div>
-    <div class="overview">
-      <h3>Overview</h3>
-      ${overview}
-    </div>
-  `;
-    main.appendChild(movieElement);
-  });
-};
-
-const getMovies = async (url) => {
-  const res = await fetch(url);
-  const data = await res.json();
-  showMovies(data.results);
-};
-
-getMovies(API_URL);
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const searchTerm = search.value;
-  if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
-    search.value = "";
-  } else history.go(0);
-});
-
-const waktu = document.getElementById("waktu")
-
-function tanggal() {
-    const date = new Date()
-    waktu.innerHTML = date;
+function tanggal() { // Fungsi untuk menampilkan waktu secara real-time
+    const date = new Date()  // Membuat objek Date yang merepresentasikan waktu saat ini
+    waktu.innerHTML = date; // Mengubah innerHTML dari elemen dengan ID "waktu" menjadi waktu saat ini
 }
 
-setInterval(tanggal, 1000);
+setInterval(tanggal, 1000); // Memanggil fungsi tanggal() setiap 1000 milidetik (1 detik) untuk menampilkan waktu secara terus-menerus
 
-tanggal();
+tanggal(); // Memanggil fungsi tanggal() sekali saat halaman dimuat untuk menampilkan waktu awal
 
-function sweetalert() {
-    Swal.fire({
+function sweetalert() { // Fungsi untuk menampilkan pop-up SweetAlert saat tombol ditekan
+    Swal.fire({  // Menampilkan pop-up SweetAlert dengan pesan "Good job!" dan ikon "success" saat tombol ditekan
         title: "Good job!",
         text: "You clicked the button!",
         icon: "success"
